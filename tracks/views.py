3df2073aa73a2122942da384d96fcfdc65a7c7d0
Yuan Chen <18861720589@163.com>
2022-08-16 14:03:16 -0500

    feat(tracks): add models

--- tracks/views.py
+++ tracks/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+
+# Create your views here.
+from tracks.models import ApplicationRecord
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    items = ApplicationRecord.objects.all()
+    # myFilter = facultyFilter(request.GET, queryset=items)
+    # items = myFilter.qs
+    context = {
+        'items': items,
+        # 'header': 'faculty',
+        # 'myFilter': myFilter,
+    }
+    return render(request, 'index.html', context)
