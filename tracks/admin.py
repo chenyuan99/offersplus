cc7d9b00f331a9053bd6d6348c02514179613199
Yuan Chen <18861720589@163.com>
2022-08-16 11:03:19 -0500

    feat(tracks): add models

---
+++ tracks/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from tracks.models import ApplicationRecord
+
+admin.site.register(ApplicationRecord)
