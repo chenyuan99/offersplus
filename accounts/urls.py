cfa7db26ec123d4b418295a9d8ec89c04b890f15
Yuan Chen <18861720589@163.com>
2023-03-20 23:56:35 -0400

    feat(accounts):update login and logout logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('logout', views.logout_request, name='logout'),
-    path('register/', views.register, name='register')
-]
+# from django.urls import path
+#
+# from . import views
+#
+# urlpatterns = [
+#     path('logout', views.logout_request, name='logout'),
+#     path('register/', views.register, name='register')
+# ]
