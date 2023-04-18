82d22f8cde98738092e28c5da10456afb30a95a1
Yuan Chen <18861720589@163.com>
2023-04-18 15:18:19 -0400

    feat(jobgpt):add job gpt logics

--- offer_plus/urls.py
+++ offer_plus/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # path("accounts/", include("django.contrib.auth.urls")),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('jobgpt/', include('jobgpt.urls')),
 ]
