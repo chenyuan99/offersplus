850a181e792f0ad0add5a43bf19d094a43be9357
cysbc1999 <cysbc1999@gmail.com>
2023-04-23 21:17:52 -0400

    feat(jobGPT):make jobGPT works in POC

--- jobgpt/urls.py
+++ jobgpt/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='jobgpt'),
+    path('prompt', views.get_prompt, name='prompt'),
 ]
--- jobgpt/views.py
+++ jobgpt/views.py
@@ -1,5 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-
+from .generator import generate_response
 # Create your views here.
 def index(request):
     return render(request, 'jobgpt/jobgpt.html')
+
+
+def get_prompt(request):
+    if not request.user.is_authenticated:
+        return JsonResponse({'prompt': 'You must be logged in to use this feature.'})
+    result = generate_response(request.POST.get('prompt'))
+    return JsonResponse({'prompt': result})
