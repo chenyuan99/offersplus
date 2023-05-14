ced75ba3a74b4cc98bcaa7e6efca00f1372bbc58
Yuan Chen <18861720589@163.com>
2023-05-14 17:33:15 -0400

    feat(companies):add companies page

--- tracks/urls.py
+++ tracks/urls.py
@@ -1,3 +1,4 @@
+from django.template.defaulttags import url
 from django.urls import path
 
 from . import views
@@ -5,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hardware', views.hardware, name='hardware'),
-    path('add-application', views.ApplicationRecordView.as_view(), name='add-application'),
+    path('companies', views.companies, name='companies'),
+    # path('add-application', views.ApplicationRecordView.as_view(), name='add-application'),
     path('application/edit/<int:id>/', views.edit_application, name='application-edit'),
+    # url(r'^add_device$', views.add_application),
 ]
