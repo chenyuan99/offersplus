a199082e6d10302026d579c84e458e3212f20098
Yuan Chen <18861720589@163.com>
2023-05-15 07:31:28 -0400

    feat(companies):add companies page

--- tracks/forms.py
+++ tracks/forms.py
@@ -6,4 +6,12 @@ from tracks.models import ApplicationRecord
 class ApplicationRecordForm(ModelForm):
     class Meta:
         model = ApplicationRecord
-        fields = ['outcome', 'job_title', 'company_name', 'application_link']
+        fields = ['outcome', 'job_title', 'company_name', 'application_link','applicant']
+
+    def __init__(self, *args, **kwargs):
+        from django.forms.widgets import HiddenInput
+        hide_condition = kwargs.pop('hide_condition', None)
+        super(ApplicationRecordForm, self).__init__(*args, **kwargs)
+        if hide_condition:
+            self.fields['applicant'].widget = HiddenInput()
+            # or alternately:  del self.fields['fieldname']  to remove it from the form altogether.
