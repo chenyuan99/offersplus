6e96b9ed94fa082141c2b0117e0fa66570a7e21c
Yuan Chen <18861720589@163.com>
2023-06-14 22:22:04 -0400

    feat(prod) add prompts templates

---
+++ company/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from tracks.models import Company, ApplicationRecord
+
+
+def display_companies(request):
+    items = Company.objects.all()
+    context = {
+        'items': items,
+    }
+    return render(request, 'company/company.html', context)
+
+
+def display_company(request, company_name):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    print(company_name)
+    items = ApplicationRecord.objects.filter(company_name=company_name)
+    for item in items: print((item.applicant.username, request.user.username))
+    # myFilter = facultyFilter(request.GET, queryset=items)
+    # items = myFilter.qs
+    context = {
+        'items': items,
+        'company': company_name
+        # 'myFilter': myFilter,
+    }
+    return render(request, 'company/company-detail.html', context)
